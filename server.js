975990cf3b2f8aaa158fa946dc78f437c2504703
Aditya Kushwaha <kushwahaaditya251@gmail.com>
2020-12-31 10:35:22 +0530

    server file changed

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ let users ={
 let socketmap={} ; 
 const io = socketio(server) ; 
 
+app.set("view engine" , "hbs") ;
+app.use(express.static("public")) ;  
+
 io.on("connection" , (socket)=>{
     console.log("connecction establishes" , socket.id) ; 
 
@@ -60,8 +63,12 @@ io.on("connection" , (socket)=>{
 })
 
 
-
-app.use("/" , express.static(__dirname+"/public")) ;
+app.get("/login" , ()=>{
+    res.render("login")
+})
+app.get("/" , (req,res)=>{
+    res.render("index") ; 
+})
 
 server.listen(2424, ()=>{
     console.log("server started on http://localhost:2424")  ; 
